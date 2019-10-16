ca99c9ad523b0b47c6432dcd64ed379185795625
zijiazhai <zijiazhai@gmail.com>
2019-10-16 18:36:55 -0500

    ThreadSafe practice, compare two ways

--- CoreDataPractice/CoreDataPractice/Company/CompaniesController.swift
+++ CoreDataPractice/CoreDataPractice/Company/CompaniesController.swift
@@ -13,6 +13,103 @@ import CoreData
 class CompaniesController: UITableViewController {
     
     var companies = [Company]()
+    
+    @objc private func test1() {
+        CoreDataManager.shared.persistentContainer.performBackgroundTask({ (backgroundContext) in
+            (0...5).forEach { (value) in
+                print(value)
+                let company = Company(context: backgroundContext)
+                company.name = String(value)
+            }
+            
+            do {
+                try backgroundContext.save()
+                
+                DispatchQueue.main.async {
+                    self.companies = CoreDataManager.shared.fetchCompanies()
+                    self.tableView.reloadData()
+                }
+                
+            } catch let err {
+                print("Failed to save:", err)
+            }
+        })
+    }
+    
+    // bad way
+    // let's do some tricky updates with core data
+    @objc private func test2() {
+        CoreDataManager.shared.persistentContainer.performBackgroundTask { (backgroundContext) in
+            let request: NSFetchRequest<Company> = Company.fetchRequest()
+            do {
+                let companies = try backgroundContext.fetch(request)
+                companies.forEach({ (company) in
+                    print(company.name ?? "")
+                    company.name = "C: \(company.name ?? "")"
+                })
+                do {
+                    try backgroundContext.save()
+                    
+                    // let's try to update the UI after a save
+                    DispatchQueue.main.async {
+                        
+                        // reset will forget all of the objects you've fetch before
+                        CoreDataManager.shared.persistentContainer.viewContext.reset()
+                        
+                        // you don't want to refetch everything if you're just simply update one or two companies
+                        
+                        self.companies = CoreDataManager.shared.fetchCompanies()
+                        
+                        // is there a way to just merge the changes that you made onto the main view context?
+                        
+                        self.tableView.reloadData()
+                    }
+                    
+                } catch let saveErr {
+                    print("Failed to save on background:", saveErr)
+                }
+                
+            } catch let err {
+                print("Failed to fetch companies on background:", err)
+            }
+        }
+    }
+    
+    // Best way
+    @objc private func test3() {
+        DispatchQueue.global(qos: .background).async {
+            let privateContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+            privateContext.parent = CoreDataManager.shared.persistentContainer.viewContext
+            
+            let request: NSFetchRequest<Company> = Company.fetchRequest()
+            request.fetchLimit = 1
+            do {
+                let companies = try privateContext.fetch(request)
+                companies.forEach({ (company) in
+                    print(company.name ?? "")
+                    company.name = "D: \(company.name ?? "")"
+                })
+                do {
+                    try privateContext.save()
+                    DispatchQueue.main.async {
+                        do {
+                            let context = CoreDataManager.shared.persistentContainer.viewContext
+                            if context.hasChanges {
+                                try context.save()
+                            }
+                            self.tableView.reloadData()
+                        } catch let finalSaveErr {
+                            print("Failed to save main context:", finalSaveErr)
+                        }
+                    }
+                } catch let saveErr {
+                    print("Failed to save on private context:", saveErr)
+                }
+            } catch let fetchErr {
+                print("Failed to fetch on private context:", fetchErr)
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
